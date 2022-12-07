c3d5be5ee48e864b2aedb200f17e08376030f29b
timothymichaelcook <tmc295@email.vccs.edu>
2022-12-07 09:31:20 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var computerChoice = document.getElementById('computer-choice');
+var userChoice = document.getElementById('user-choice');
+var resultDisplay = document.getElementById('result');
+var possibleChoices = document.getElementById('button');
+
+po
