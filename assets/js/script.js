6a3296559a98a8f93bde6633df20cfcf4e351ab4
timothymichaelcook <tmc295@email.vccs.edu>
2022-12-07 21:59:06 -0500

    declared variables in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,47 @@
-var computerChoice = document.getElementById('computer-choice');
-var userChoice = document.getElementById('user-choice');
-var resultDisplay = document.getElementById('result');
-var possibleChoices = document.getElementById('button');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice
+let computerChoice
+let result
 
-po
+possibleChoices.forEach(possibleChoices => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id 
+    playerChoice.innerHTML = userChoice
+    generateComputerChoice
+}))
+
+
+function generateComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * possibleChoices) + 1
+
+    if (randomNumber === 1) {
+        computerChoice = "Rock"
+    }
+    if (randomNumber === 2) {
+        computerChoice = "Paper"
+    }
+    if (randomNumber === 3) {
+        computerChoice = "Scissors"
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = "It's a draw!"
+    }
+    if (computerChoice === userChoice) {
+        result = "You win!"
+    }
+    if (computerChoice === userChoice) {
+        result = "You win!"
+    }
+    if (computerChoice === userChoice) {
+        result = "You lose!"
+    }
+    if (computerChoice === userChoice) {
+        result = "You lose!"
+    }
+}
