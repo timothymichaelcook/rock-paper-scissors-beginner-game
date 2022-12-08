9b41b77620fe935118e2d19c2c063b8047d1d9f2
timothymichaelcook <tmc295@email.vccs.edu>
2022-12-08 16:07:29 -0500

    code is working, still need to find better debugging methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,64 @@
-const computerChoiceDisplay = document.getElementById('computer-choice');
-const userChoiceDisplay = document.getElementById('user-choice');
-const resultDisplay = document.getElementById('result');
-const possibleChoices = document.querySelectorAll('button');
+// DECLARE VARIABLES ()
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let randomNumber
 let userChoice
 let computerChoice
 let result
 
-possibleChoices.forEach(possibleChoices => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id 
-    playerChoice.innerHTML = userChoice
-    generateComputerChoice
-}))
 
-
-function generateComputerChoice () {
-    const randomNumber = Math.floor(Math.random() * possibleChoices) + 1
-
+//DECLARE FUNCTIONS
+function generateComputerChoice() {
+    randomNumber = Math.floor(Math.random() * 3) // or you can use possibleChoices.length
+    
+    if (randomNumber === 0) {
+      computerChoice = 'rock'
+    }
     if (randomNumber === 1) {
-        computerChoice = "Rock"
+      computerChoice = 'scissors'
     }
     if (randomNumber === 2) {
-        computerChoice = "Paper"
-    }
-    if (randomNumber === 3) {
-        computerChoice = "Scissors"
+      computerChoice = 'paper'
     }
     computerChoiceDisplay.innerHTML = computerChoice
-}
+  }
 
 function getResult() {
     if (computerChoice === userChoice) {
-        result = "It's a draw!"
+        result = 'its a draw!'
     }
-    if (computerChoice === userChoice) {
-        result = "You win!"
+    if (computerChoice === "rock" && userChoice === "paper") {
+        result = 'you win!'
     }
-    if (computerChoice === userChoice) {
-        result = "You win!"
+    if (computerChoice === "rock" && userChoice === "scissors") {
+        result = 'you lose...'
     }
-    if (computerChoice === userChoice) {
-        result = "You lose!"
+    if (computerChoice === "paper" && userChoice === "scissors") {
+        result = 'you win!'
     }
-    if (computerChoice === userChoice) {
-        result = "You lose!"
+    if (computerChoice === "paper" && userChoice === "rock") {
+        result = 'you lose...'
     }
+    if (computerChoice === "scissors" && userChoice === "rock") {
+        result = 'you win!'
+    }
+    if (computerChoice === "scissors" && userChoice === "paper") {
+        result = 'you lose...'
+    }
+    resultDisplay.innerHTML = result
 }
+
+// MAIN CODE 
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id 
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+
+
+
+
